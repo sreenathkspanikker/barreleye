b5a18d762a9aba19722cc28c9ad52f112de17b54
Sreenath Kalarikkal Sreekumar <sreenath.sreekumar@accenture.com>
2023-03-12 13:54:21 +0200

    update 017

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,50 @@
-import { Layout, Section } from "../components";
+import Image from "next/image";
+import { Col, Figure, Row } from "react-bootstrap";
+import { Layout, Title } from "../components";
+
+import AboutJson from "../utils/about.json";
 
 export default function About() {
 
     return (
         <Layout className="page-about">
-            <h1>About</h1>
+            {AboutJson?.data?.map((items, idx) => (
+                <div className="list-item" key={idx}>
+                    <Row>
+                        <Col sm={6}>
+                            <Figure>
+                                <Image
+                                    src={require(`../assets/images/${items.image}`)}
+                                    className="img-fluid"
+                                    alt="about" />
+                            </Figure>
+                        </Col>
+                        <Col sm={6}>
+                            <Title title={items?.title} />
+                            <p>{items?.description}</p>
+                        </Col>
+                    </Row>
+                </div>
+            ))}
+
+            <div className="about-team">
+                <Title
+                    title="Barreleye Tree"
+                    subTitle="Lörem ipsum pladat prev. Aplande biovalens. Kontratt tigisk, i egokäna om göss dinde, hexanymir. Geongar megapåra mans att decibel. Hev or i dijön social turism dumpling. Sagt terasm nev så hexafalig, i plajolig samt d"
+                />
+                <div className="team-list">
+                    {AboutJson?.team?.map((items, idx) => (
+                        <Figure key={idx}>
+                            <Image
+                                src={require(`../assets/images/${items.image}`)}
+                                className="img-fluid"
+                                alt="about" />
+                            <h3>{items?.title}</h3>
+                            <p>{items?.name}</p>
+                        </Figure>
+                    ))}
+                </div>
+            </div>
         </Layout>
     )
 }
